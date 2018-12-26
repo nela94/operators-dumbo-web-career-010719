a8214b0013716f7e6789c5e5c2b01240ab09bf36
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-12-26 16:40:13 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,15 @@
 def unsafe?(speed)
  if speed > 60
-	 return true 
+	 return true
  elsif speed < 40
-	 return true 
- else 
-	 return false 
- end 	    
+	 return true
+ else
+	 return false
+ end
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
